0719cd7b359067317c6f5c4ac0fbcc1c877313a5
霍维国 <huoweiguo@huoweiguodeMacBook-Pro.local>
2018-05-28 10:16:12 +0800

    add contact page

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require("express"),
 	component = require("./controller/component"),
 	app = express();
 
-app.use(express.static(path.join(__dirname,"public")));
-app.set("view engine","ejs");	
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
-app.get("/",component.guide);
+app.get("/", component.guide);
 
-app.get("/login",component.loginReg)
+app.get("/contact", function (req, res, next) {
+	res.render('这是联系我们板块');
+});
+
+app.get("/login", component.loginReg)
 
 app.listen(3000);
